54513cb99b384c7a2fce633568bc1112fd2c5e10
Geremias Lucban <glucban@yondu.com>
2023-10-22 02:07:18 +0800

    enhance ui

--- react-to-do-app/src/components/TaskListItem.tsx
+++ react-to-do-app/src/components/TaskListItem.tsx
@@ -13,18 +13,18 @@ interface TaskListItemProps {
 
 const TaskListItem: React.FC<TaskListItemProps> = ({ task, onRemove, onCompletion }) => {
   return (
-    <li className={`flex tasks-center justify-between mb-2`}>
-      <span
-        className={`cursor-pointer ${task.is_completed ? 'line-through' : ''}`}
-        onClick={() => onCompletion(task.id)}
-        title={task.is_completed ? 'Completed Task' : 'Click to mark as completed'}
-      >
-        {task.description}
-      </span>
-      <button className="text-red-500 mt-2 sm:mt-0 sm:ml-2" onClick={() => onRemove(task.id)}>
-        Remove
-      </button>
-    </li>
+    <li className={`mb-2 bg-gray-200 rounded-lg p-4 flex items-center justify-between`}>
+    <span
+      className={`cursor-pointer flex-1 ${task.is_completed ? 'line-through' : ''}`}
+      onClick={() => onCompletion(task.id)}
+      title={task.is_completed ? 'Completed Task' : 'Click to mark as completed'}
+    >
+      {task.description}
+    </span>
+    <button className="text-red-500 ml-2" onClick={() => onRemove(task.id)}>
+      Remove
+    </button>
+  </li>
 
   );
 };
